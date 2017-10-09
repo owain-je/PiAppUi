5561cba0d667f2157346c3f05b21586079a8b723
owain perry <owain.perry@just-eat.com>
2017-10-09 13:16:25 +0100

    added health controller

---
+++ PiUi/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PiUi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Health")]
+    public class HealthController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "ok";
+        }
+    }
+}
